f78313e5e0f66c1656d372d8238570b067356d33
Jason Tung <mickeu420@gmail.com>
2018-10-23 17:50:58 -0400

    fixed it all

--- fileio.c
+++ fileio.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 int main(void) {
-    int fileint = open("example.txt", O_RDONLY);
+    int fileint = open("example.txt", O_CREAT | O_RDWR | O_APPEND, S_IWUSR | S_IXUSR | S_IRUSR);
     printf("fileint: %d\n", fileint);
     if (fileint > 0){
         write(1, "text opened\n", sizeof(char) * strlen("text opened\n"));
@@ -16,13 +16,16 @@ int main(void) {
         if (read(fileint, holder, 999) < 0){
             write(2, "read error\n", sizeof(char) * strlen("read error\n"));
         }
-        printf("this is what's in the file: %s", holder);
+        printf("this is what's in the file: %s\n", holder);
 
-        if (write(fileint, "henlo\n", 6) != 6) {
+        if (write(fileint, "herooooo", 6) != 6) {
             write(2, "write error\n", sizeof(char) * strlen("write error\n"));
         }
+        else{
+            write(1, "i wrote herro into it\n", sizeof(char) * strlen("i wrote herro into it\n"));
+        }
 
-        if(close(fileint) > 0) {
+        if(close(fileint) >= 0) {
             write(1, "okay cool it closed\n", sizeof(char) * strlen("okay cool it closed\n"));
         }
         else{
